956f2498fe2775220b78d6cf99e8b0f8c665f6c8
Matanef <efratimatan@gmail.com>
2023-12-19 16:36:33 +0200

    Week_4_Day_2_ExerciseGold_exercise2_&_3

--- Week_4/Day_2/ExerciseGold/script.js
+++ Week_4/Day_2/ExerciseGold/script.js
@@ -7,19 +7,19 @@
 // console.log(isBlank('abc')); --> false
 
 
-function ifBlank(){
-    let stringOrNot;
-    do {
-        stringOrNot = prompt("please enter a string");
-        // console.log(stringOrNot);
-    } while (!isNaN(stringOrNot));
-    console.log(stringOrNot);
-        return true;
+// function ifBlank(){
+//     let stringOrNot;
+//     do {
+//         stringOrNot = prompt("please enter a string");
+//         // console.log(stringOrNot);
+//     } while (!isNaN(stringOrNot));
+//     console.log(stringOrNot);
+//         return true;
 
-}
+// }
 
-let a = ifBlank();
-console.log(a);
+// let a = ifBlank();
+// console.log(a);
 
 // Exercise 2 : Abbrev_name
 // Instructions
@@ -30,5 +30,38 @@ console.log(a);
 
 
 function abbrevName(firstname, lastname){
+    let firstNameInitials = firstname[0];
+    let lastNameInitials = lastname[0];
+    let abbname = `${firstNameInitials}.  ${lastNameInitials}.`
+    return abbname
+}
+
+let b = abbrevName("Matan", "Efrati");
+console.log(b);
+
+// Exercise 3 : SwapCase
+// Instructions
+
+// Write a JavaScript function which takes a string as an argument and swaps the case of each character.
+// For example :
+
+// if you input 'The Quick Brown Fox' 
+// the output should be 'tHE qUICK bROWN fOX'.
+
+function SwapCase(inputString){
+    let changeChar ="";
+for (let i = 0; i<inputString.length; i++){
+    if (inputString[i] === inputString[i].toUpperCase()){
+            changeChar += inputString[i].toLowerCase();
+    }else {
+        changeChar += inputString[i].toUpperCase();
+        }
+        
+}
+return(changeChar);
 
 }
+let c = SwapCase("This Is A String")
+console.log(c);
+
+
