e22077ee87a5bfc123e1d90273fe5d1b91e7bf75
Matanef <efratimatan@gmail.com>
2023-12-20 23:52:12 +0200

    Week_4_Day_4_ExrciseXP_execrcise4_just_need_to_change_color_to_red

--- Week_4/Day_4/ExerciseXP/Exercise4/script.js
+++ Week_4/Day_4/ExerciseXP/Exercise4/script.js
@@ -3,13 +3,13 @@ const allBooks = []
 const book1 = {
         title: "Lord Of the Rings",
         author: "J. R. R. Tolkien",
-        image: "https://picsum.photos/200",
+        image: "https://tolkiengateway.net/w/images/thumb/c/c3/The_Lord_of_the_Rings_1954-55.png/800px-The_Lord_of_the_Rings_1954-55.png",
         alreadyRead: "true"
     }
 const book2= {
         title: "The Prince",
         author: "Niccolò Machiavelli",
-        image: "https://en.wikipedia.org/wiki/The_Prince#/media/File:Machiavelli_Principe_Cover_Page.jpg",
+        image: "https://m.media-amazon.com/images/I/4117w9ptZnL._AC_UF894,1000_QL80_.jpg",
         alreadyRead: "true"    
     }
 allBooks.push(book1, book2)
@@ -31,10 +31,26 @@ table.innerHTML = `
 </td>
 <td>Already read: ${book1.alreadyRead}</td>
     </tr>
+    <tr>
+    <td>${book2.title} Written by ${book2.author}</td>
+<td>
+<img src="${book2.image}">
+</td>
+<td>Already read: ${book2.alreadyRead}</td>
+    </tr>
+    
 </tbody
 `
 const bookListDiv = document.querySelector(".list-books")
 console.log('bookListDiv:', bookListDiv);
-bookListDiv.append(table)
+bookListDiv.append(table);
+
+//tried to do it from here but wasn;t able to so used CSS style.
+// const imgSize = document.getElementsByTagName("img");
+// imgSize.style.width = "100px";
+// console.log(imgSize);
 
 
+if (book1.alreadyRead ==="true"){
+    
+}
