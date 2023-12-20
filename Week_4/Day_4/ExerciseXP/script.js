0293ff81b45adba49a1312af95ba1e84d02b0ae8
Matanef <efratimatan@gmail.com>
2023-12-20 20:09:30 +0200

    Week_4_Day_4_ExrciseXP_execrcise1_with_bonus_done

--- Week_4/Day_4/ExerciseXP/script.js
+++ Week_4/Day_4/ExerciseXP/script.js
@@ -7,7 +7,7 @@ let myDiv = document.querySelector("div")
 console.log(myDiv);
 
 const myUl1=document.getElementsByTagName("ul");
-console.log(myUl1[0]);
+console.log(myUl1[0], myUl1[1]);
 
 const secondLi = document.querySelector("li:last-child");
 console.log(secondLi);
@@ -15,12 +15,38 @@ console.log(secondLi);
 const liPete = document.querySelector("ul.list:nth-child(2) li:last-child").innerHTML= "Richard";
 console.log(liPete);
 
-const myUl2=document.getElementsByTagName("ul");
-console.log(myUl2[1]);
-
-// const liSarah = document.querySelector(`ul.list:nth-child(1) li:nth-child(1)`)
-// liSarah.remove();
+// const myUl2=document.getElementsByTagName("ul");
+// console.log(myUl2[1]);
 
 const liSarah = document.querySelector("ul.list:nth-child(3) li:nth-child(2)")
 console.log(liSarah);
-liSarah.remove()
+liSarah.remove();
+
+
+const firstName = "Matan";
+const ulElements = document.querySelectorAll(`ul`);
+console.log(ulElements);
+
+for (let i=0; i<ulElements.length; i++){
+    const ulElement = ulElements[i];
+    console.log(ulElement);
+    const liFirstName = ulElement.querySelector(`li:first-child`);
+    console.log(liFirstName);
+    liFirstName.textContent = firstName
+}
+
+///That was an annoying exercise!
+
+for (let i=0; i<ulElements.length; i++){
+const ulElementsAddClass = ulElements[i];
+console.log(ulElementsAddClass);
+
+ulElementsAddClass.classList.add("student_list");
+}
+
+
+const ulElementsAddingTwoClass = document.querySelector("ul");
+console.log(ulElementsAddingTwoClass);
+    
+ulElementsAddingTwoClass.classList.add("university", "attendance");
+
