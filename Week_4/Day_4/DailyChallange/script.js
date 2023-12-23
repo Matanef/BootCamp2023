ff33dd7f7238f71d48e6268c590681817ba4c28c
Matanef <efratimatan@gmail.com>
2023-12-24 00:52:01 +0200

    Week_4_Day_4_dailyChallange_panets_done_moons_Done_explanation

--- Week_4/Day_4/DailyChallange/script.js
+++ Week_4/Day_4/DailyChallange/script.js
@@ -1,3 +1,4 @@
+//created array with objects which each have 3 keys. colors in hex.
 let solarArray = [
     {name: 'Mercury', moons: 0, color: "#c8c3cc"},
     {name: 'Venus', moons: 0, color: "#ff7b25"},
@@ -10,28 +11,36 @@ let solarArray = [
     {name: 'Pluto', moons: 5, color: "#cab577"}
     ]
 
-
+//accessed the section tag and saved it into a variable.
     let planetSection = document.querySelector('section');
     console.log(planetSection);
-
+//created a loop to go through the array's objects with element planet to be able to edit all of them togther.
     for (let planet of solarArray){
+        //created a div tag.
         let divPlanets = document.createElement('div');
+        //added the class "planet" to the div tag"
         divPlanets.classList.add('planet');
+        //thr loop will name each "planet" with the objects in the array.
         divPlanets.innerText = planet.name;
+        //again, giving each "planet" the propertie color
         divPlanets.style.backgroundColor = planet.color;
+        //and here the div tag is inserted into the section tag.
         planetSection.appendChild(divPlanets)
-        console.log(divPlanets);
+        // console.log(divPlanets);
 
-            // let moons = planet.moon;
+        //kind of doing the same thing with the moons iside the planets loop.
     for (let moons of solarArray){
+        //creating a div tag for the moons
         let divMoons = document.createElement('div');
+        //adding class of moons the the new div.
         divMoons.classList.add('moon');
+        //editing the inner text to the amount of moons which is the key in the objects.
         divMoons.innerText = planet.moons;
         divMoons.style.left = "250px"
         divMoons.style.backgroundColor = planet.color;
         divMoons.style.textAlign = "center"
         planetSection.appendChild(divMoons)
-    console.log(divMoons);
+    // console.log(divMoons);
     }
     };
 
