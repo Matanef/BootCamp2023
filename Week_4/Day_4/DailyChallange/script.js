4804e387d214bbabd2c64816803178329b137299
Matanef <efratimatan@gmail.com>
2023-12-23 22:33:18 +0200

    Week_4_Day_4_dailyChallange_panets_done_moons_no

---
+++ Week_4/Day_4/DailyChallange/script.js
@@ -0,0 +1,35 @@
+let solarArray = [
+    {name: 'Mercury', moons: 0, color: "#c8c3cc"},
+    {name: 'Venus', moons: 0, color: "#ff7b25"},
+    {name: 'Earth', moons: 1, color: "blue"},
+    {name: 'Mars', moons: 2, color: "#c94c4c"},
+    {name: 'Jupiter', moons: 79, color: "#bc5a45"},
+    {name: 'Saturn', moons: 82, color: "#dac292"},
+    {name: 'Uranus', moons: 27, color: "#b7d7e8"},
+    {name: 'Neptun', moons: 14, color: "#034f84"},
+    {name: 'Pluto', moons: 5, color: "#cab577"}
+    ]
+
+
+    let planetSection = document.querySelector('section');
+    console.log(planetSection);
+
+    for (let planet of solarArray){
+        let divPlanets = document.createElement('div');
+        divPlanets.classList.add('planet');
+        divPlanets.innerText = planet.name;
+        divPlanets.style.backgroundColor = planet.color;
+        planetSection.appendChild(divPlanets)
+        console.log(divPlanets);
+    };
+
+    // let moons = planet.moon;
+    for (let moons of solarArray){
+        let divMoons = document.createElement('div');
+        divMoons.classList.add('moon');
+        divMoons.innerText = planet.moons;
+        moonDivs.style.left = "250px"
+        divPlanets.style.backgroundColor = divPlanets;
+        planetSection.appendChild(divMoons)
+    console.log(divMoons);
+    }
---
+++ Week_4/Day_5_MiniProject/DailyChallange/script.js
@@ -0,0 +1,5 @@
+let userNumber;
+do {
+    userNumber = parseInt(prompt("let's sing along, With how many bottles should we start?"));
+}while (userNumber <=0 || isNaN(userNumber));
+
