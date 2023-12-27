101503dfbc965d53bb3270fb3dbfa2192e72752d
Matanef <efratimatan@gmail.com>
2023-12-27 23:54:16 +0200

    Week_5_Day_4_Exercise_1_part_3_done

--- Week_5/Day_4/ExerciseXP/Exercise_1/script.js
+++ Week_5/Day_4/ExerciseXP/Exercise_1/script.js
@@ -13,3 +13,17 @@
 // }
 
 // setTimeout(appPara, 2000);
+
+let divcontainer = document.getElementById('container');
+const clearButton = document.getElementById('clear');
+
+function newAppPara(){
+    const newPara = document.createElement('p');
+    newPara.innerText = "Hello World";
+    divcontainer.appendChild(newPara);
+};
+
+intervalId = setInterval(newAppPara, 2000);
+clearButton.addEventListener('click', function(){
+    clearInterval(intervalId);
+});
