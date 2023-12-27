03c3b278e69ba94010a56fdd3c13db64e9a30a0c
Matanef <efratimatan@gmail.com>
2023-12-27 19:21:50 +0200

    Week_5_Day_2_Exercise_XP_Exercise_3_Done

---
+++ Week_5/Day_2/ExerciseXP/Exercise_3/script.js
@@ -0,0 +1,28 @@
+let allBoldItems;
+
+function getBoldItems(){
+    allBoldItems = document.querySelectorAll('strong')
+    for (let i = 0 ; i<allBoldItems.length ; i++)
+    return allBoldItems;
+}
+console.log(getBoldItems());
+
+let strongCell = document.getElementsByTagName('strong');
+console.log(strongCell);
+
+function highlight(){
+
+    for (let i =0; i<strongCell.length; i++){
+        strongCell[i].style.backgroundColor = "blue"
+        strongCell[i].style.color = "white"
+    }
+}
+// highlight()
+
+function returnItemsToDefault(){
+    let strongBackToWBlack= getBoldItems()
+    for (let i = 0 ; i <strongBackToWBlack.length; i++){
+        strongBackToWBlack[i].style.color = 'black'
+    }
+}
+// returnItemsToDefault()
