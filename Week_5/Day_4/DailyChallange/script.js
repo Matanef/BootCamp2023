3998331f0816c357346eeaf6b18b302f7074e4d3
Matanef <efratimatan@gmail.com>
2023-12-28 14:07:15 +0200

    Week_5_Day_4_DailyChallange_added_input_for_numbers

--- Week_5/Day_4/DailyChallange/script.js
+++ Week_5/Day_4/DailyChallange/script.js
@@ -16,3 +16,25 @@ input.addEventListener('input', function(event){
     let onlyLetters = inputValue.replace(/[^a-zA-Z]/g, '');
     event.target.value = onlyLetters;
 })
+
+
+let input2 = document.createElement('input');
+let labelInput2 = document.createElement('label');
+
+labelInput2.innerText = 'Please enter only numbers: ';
+labelInput2.style.display = 'block';
+input2.style.display = 'block';
+body.appendChild(labelInput2)
+body.appendChild(input2)
+console.log(input2);
+
+input2.addEventListener('input', function(event){
+    let input2Value = event.target.value;
+    let OnlyNumbers = inputValue.replace(/[^a-zA-Z]/g, '');
+    if(input2Value === '' || isNaN(input2Value)){
+        alert('This is not a valid input')
+        
+    }else{
+        console.log(input2Value* 5);
+    }
+})
