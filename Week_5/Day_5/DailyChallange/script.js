b74b3c386872234597b75cde4763d1d2c045981b
Matanef <efratimatan@gmail.com>
2023-12-31 00:43:34 +0200

    Week_5_Day_5_DailyChallange_input_ok_cant_see_fontAwasome

--- Week_5/Day_5/DailyChallange/script.js
+++ Week_5/Day_5/DailyChallange/script.js
@@ -3,7 +3,9 @@ const tasks = [];
 
 function addTask(){
     const inputUser = document.getElementById('inputUser');
-    const taslList = document.getElementById('taskList');
+    console.log(inputUser);
+    const taskList = document.getElementById('taskList');
+    console.log(taskList);
 
     const taskText = inputUser.value.trim();
     if(taskText !==''){
@@ -11,6 +13,32 @@ function addTask(){
     const taskObject = {taskId: taskId,
                         text: taskText,
                         done: false};
+    tasks.push(taskObject)
+    const taskDiv = document.createElement('div');
+    taskDiv.classList.add('task');
+    
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkbox'
+    checkBox.id = `checkbox_${taskId}`;
+    // checkBox.addEventListener('change', doneTask(taskId));
+
+    const label = document.createElement('label');
+    label.setAttribute('for', `checkbox_${taskId}`);
+    label.textContent= taskText;
+    
+    const deleteButton  = document.createElement('button');
+    deleteButton.innerHTML =  `<i class="fa-regular fa-circle-xmark"></i>`;
+    // deleteButton.addEventListener('click', deleteTask(taskId));
+    
+    taskDiv.appendChild(checkBox);
+    taskDiv.appendChild(label);
+    taskDiv.appendChild(deleteButton);
+
+    taskList.appendChild(taskDiv);
+
+    inputUser.value = '';
+    console.log(tasks)
     }
+    
     console.log(tasks);
 }
