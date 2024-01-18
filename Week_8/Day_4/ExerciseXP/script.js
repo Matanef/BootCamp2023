47f281115adf11447d6c56318348dae6c3b83706
Matanef <efratimatan@gmail.com>
2024-01-18 02:10:29 +0200

    Week 8 Day 4 ExrciseXP exercise 4 one done

--- Week_8/Day_4/ExerciseXP/script.js
+++ Week_8/Day_4/ExerciseXP/script.js
@@ -54,3 +54,15 @@ let simplestring = epic.reduce((reduce, sentence) => {
 });
 console.log(simplestring);
 
+const students = [{name: "Ray", course: "Computer Science", isPassed: true}, 
+               {name: "Liam", course: "Computer Science", isPassed: false}, 
+               {name: "Jenner", course: "Information Technology", isPassed: true}, 
+               {name: "Marco", course: "Robotics", isPassed: true}, 
+               {name: "Kimberly", course: "Artificial Intelligence", isPassed: false}, 
+               {name: "Jamie", course: "Big Data", isPassed: false}];
+
+const passd = students.filter((pass) =>{
+    return pass.isPassed
+});
+console.log(passd);
+
