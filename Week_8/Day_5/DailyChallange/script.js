4b82d6fcc20861982c0f286166d1f182f3963bc4
Matanef <efratimatan@gmail.com>
2024-01-20 23:55:20 +0200

    Week 8 Day 5 DailyChallange not working

---
+++ Week_8/Day_5/DailyChallange/script.js
@@ -0,0 +1,12 @@
+function allThuthy() {
+    for(let i = 0; i<arguments.length ;i++){
+        if(!arguments)
+    }
+}
+const result = allTruthy(true, 1, 'hello',);
+console.log(result);
+const result2 = allTruthy(true, 1, 'hello',0,0,0,0,0,0);
+console.log(result2);
+const result3 = allTruthy(5, 4, 3, 2, 1, 0);
+console.log(result3);
+
