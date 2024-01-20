2a43607c13229ae5cc0b872863fc95b72efd7fbe
Matanef <efratimatan@gmail.com>
2024-01-21 00:10:42 +0200

    Week 8 Day 5 DailyChallange working

--- Week_8/Day_5/DailyChallange/script.js
+++ Week_8/Day_5/DailyChallange/script.js
@@ -1,8 +1,8 @@
-function allThuthy() {
-    for(let i = 0; i<arguments.length ;i++){
-        if(!arguments)
-    }
-}
+// function allThuthy() {
+//     for(let i = 0; i<arguments.length ;i++){
+//         if(!arguments)
+//     }
+// }
 const result = allTruthy(true, 1, 'hello',);
 console.log(result);
 const result2 = allTruthy(true, 1, 'hello',0,0,0,0,0,0);
@@ -10,3 +10,13 @@ console.log(result2);
 const result3 = allTruthy(5, 4, 3, 2, 1, 0);
 console.log(result3);
 
+function allTruthy(){
+    let result = true;
+    const argArray = [...arguments];
+    argArray.forEach(element => {
+        if(!element){
+            result = false;
+        }
+    });
+    return result
+}
