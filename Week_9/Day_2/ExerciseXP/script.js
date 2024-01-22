940043ba3fcffc4bbc7e17de6adb57ba53733bab
Matanef <efratimatan@gmail.com>
2024-01-23 00:15:59 +0200

    Week 9 Day 2 ExerciseXP 1, 2 done + Practice

--- Week_9/Day_2/ExerciseXP/script.js
+++ Week_9/Day_2/ExerciseXP/script.js
@@ -36,9 +36,14 @@ const users = { user1: 18273,
             };
 const {user1, user2, user3} = users;
 console.log(user1);
-console.log(Object.keys(users));
+const usersKeys = Object.keys(users)
+console.log(`This is users keys ${usersKeys}`);
+console.log(usersKeys);
+console.log();
 console.log(Object.values(users));
-console.log(Object.entries(users));
+const usersEntries = Object.entries(users)
+console.log(`this is user entries ${usersEntries}`);
+console.log(usersEntries);
 
 
 function arrangeTOarray(){
@@ -49,5 +54,10 @@ function arrangeTOarray(){
 arrangeTOarray()
 
 
-const newArray = Object.entries(users).map(([name, number]) => `[${name}:${number}]`);
-console.log(newArray);
+const arr1Array = Object.entries(users).map(([name, value]) => `[${name}:${value}]`);
+console.log(arr1Array);
+
+const arr2Array = Object.entries(users).map(([name, value]) => `[${name}:${value*2}]`);
+console.log(arr2Array);
+
+
