1b97f34e0fbbb01f98cf5e5abac11f72d9d670f2
Matanef <efratimatan@gmail.com>
2024-01-22 23:45:48 +0200

    Week 9 Day 2 ExerciseXP 1, 2 done

---
+++ Week_9/Day_2/ExerciseXP/script.js
@@ -0,0 +1,50 @@
+const callMe = () => {
+    date = new Date
+    return date;
+};
+const print = callMe();
+console.log(print);
+
+const person = {
+    name: 'John Doe',
+    age: 25,
+    location: {
+        country: 'Canada',
+        city: 'Vancouver',
+        coordinates: [49.2827, -123.1207]
+    }
+}
+
+const {name, location: {country, city, coordinates: [lat, lng]}} = person;
+
+console.log(`I am ${name} from ${city}, ${country}. Latitude(${lat}), Longitude(${lng})`);
+
+function displayStudentInfo(objUser){
+    const {name, age, location: {country, city,coordinates: [lat, lng] }} = person;
+    let inputName = objUser
+    console.log(objUser);
+    const {first, last} = objUser
+    console.log(`Your full name is ${first} ${last}`);
+
+}
+
+displayStudentInfo({first: 'Elie', last:'Schoppik'});
+
+const users = { user1: 18273,
+                user2: 92833,
+                user3: 90315 
+            };
+const {user1, user2, user3} = users;
+console.log(user1);
+console.log(Object.keys(users));
+console.log(Object.values(users));
+console.log(Object.entries(users));
+
+
+function arrangeTOarray(){
+    for(let [key,value] of Object.entries(users)){
+        console.log(key, value);
+    }
+}
+
+arrangeTOarray()
