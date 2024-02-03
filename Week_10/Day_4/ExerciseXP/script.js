5d3c55586256e708b41cc9852c3707d8aa38a814
Matanef <efratimatan@gmail.com>
2024-02-04 01:11:26 +0200

    Week 10 Day 5 miniproject done

--- Week_10/Day_4/ExerciseXP/script.js
+++ Week_10/Day_4/ExerciseXP/script.js
@@ -58,3 +58,21 @@ const getGif = () => {
 }
 
 getGif();
+
+
+
+function getTheGETRequest() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('resolved');
+        }, 2000);
+    });
+}
+
+async function asyncCall() {
+    console.log('calling');
+    let result = await resolveAfter2Seconds();
+    console.log(result);
+}
+
+asyncCall();
