c070df8dea9e8c901994698f13ff5c78997f9904
Matanef <efratimatan@gmail.com>
2024-02-12 16:57:49 +0200

    Week 11 day 2 Exercise_6 done

--- Week_11/Day_2/ExerciseXP/Exercise_5/math-app/app.js
+++ Week_11/Day_2/ExerciseXP/Exercise_5/math-app/app.js
@@ -1,5 +1,11 @@
-const loadsh = require('loadsh')
+const _ = require('loadsh')
 const math = require("./math.js")
+
 
 math.addition(2,3)
 math.multiplication(4,7)
+const num = _.random(0,20)
+console.log(num);
+
+// I don't understand why we needed to require loadsh if we don't use it in the exercise.
+// the const num = _.random(0,20) is just me trying to understand what to do with it.
---
+++ Week_11/Day_2/ExerciseXP/Exercise_6/npm-beginner/app.js
@@ -0,0 +1,3 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue(`I don't trust anything that bleeds for 5 days and doesn't DIE !\n "Mister Garrison"`));
