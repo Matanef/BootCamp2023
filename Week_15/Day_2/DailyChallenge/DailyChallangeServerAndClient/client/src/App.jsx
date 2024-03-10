90f3e5ccc2b4829dc40477c987681cfba1b28b0e
Matanef <efratimatan@gmail.com>
2024-03-10 17:04:09 +0200

    DailyChallenge still don't understand why not working

--- Week_15/Day_2/DailyChallenge/DailyChallangeServerAndClient/client/src/App.jsx
+++ Week_15/Day_2/DailyChallenge/DailyChallangeServerAndClient/client/src/App.jsx
@@ -1,29 +1,55 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      inputValue: '',
-      serverResponse: ''
+      inputValue: "",
+      serverResponse: "",
     };
   }
 
+
+  //handle the first response of the server which supposed to be "Hello from express"
+  componentDidMount() {
+    //Calling the callback function
+    this.fetchServerResponse();
+  }
+
+  //callback function
+  fetchServerResponse = async () => {
+    try {
+      //waiting for server response to get the data
+      const response = await fetch("/api/hello");
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("Faild to fetch messege from server");
+      }
+      //
+      const data = await response.text();
+      console.log(data);
+      //issue with the recieved data
+      this.setState({ serverResponse: data });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+//handling the sumbit event
   handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('/api/world', {
-        method: 'POST',
+      const response = await fetch("/api/world", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ value: this.state.inputValue })
+        body: JSON.stringify({ value: this.state.inputValue }),
       });
       const data = await response.text();
       this.setState({ serverResponse: data });
     } catch (error) {
-      console.log('error', error);
+      console.log("error", error);
     }
   };
 
@@ -34,10 +60,15 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <p>{this.state.serverResponse}</p>
         <h1>ReactApp</h1>
         <form onSubmit={this.handleSubmit}>
-          <input type='text' value={this.state.inputValue} onChange={this.handleChange} />
-          <button type='submit'>Submit</button>
+          <input
+            type="text"
+            value={this.state.inputValue}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Submit</button>
         </form>
         {this.state.serverResponse && (
           <p>Server response: {this.state.serverResponse}</p>
--- Week_15/Day_2/DailyChallenge/DailyChallangeServerAndClient/server.js
+++ Week_15/Day_2/DailyChallenge/DailyChallangeServerAndClient/server.js
@@ -1,15 +1,17 @@
-import express from 'express';
+const express = require('express')
 const app = express();
 
+app.get("/api/hello", (req, res) => {
+  res.send("Hello from express");
+});
 
-app.get('/api/hello', (res, req) =>{
-    res.send('Hello from express')
+app.post('/', (req,res)=> {
+    const { serverResponse } = req.body;
+    push({serverResponse})
+    res.status(201).send('new task was created')
 })
 
-
-
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}`);
-})
-
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
