e4f35ef4647c12835628d08c490b7f901863e228
Matanef <efratimatan@gmail.com>
2024-03-10 15:50:44 +0200

    DailyChallenge don't understand why not working

---
+++ Week_15/Day_2/DailyChallenge/DailyChallangeServerAndClient/client/src/App.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './App.css'
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      inputValue: '',
+      serverResponse: ''
+    }
+
+    handleSubmit = async(e) => {
+      e.preventDefault();
+      try{
+        const response = await fetch('/api/world', {
+          method: 'POST',
+          headers:{
+          'Content-Type' : 'application/json'
+          },
+          body: JSON.stringify({value: this.state.inputValue })
+        });
+          const data = await response.text();
+          this.setState({ serverResponse: data })
+      }catch(error){
+        console.log('error' , error);
+      }
+    };
+    handleChange = (e) =>{
+      this.setState({inputValue: e.target.value});
+    };
+  }
+  render(){
+    return(
+      <>
+      <div>
+        <h1>ReactApp</h1>
+        <form>
+          <input type='text' value={this.state.inputValue} onChange={this.handleChange} />
+          <button type='submit'>Submit</button>
+        </form>
+        {this.state.serverResponse && (
+          <p>Server response: {this.state.serverResponse}</p>
+        )}
+      </div>
+      </>
+    )
+  }
+}
+
+export default App
