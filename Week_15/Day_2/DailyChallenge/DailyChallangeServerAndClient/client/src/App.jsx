5b1db0fffd5266150c69aefc4a3faeb7ac5b39b8
Matanef <efratimatan@gmail.com>
2024-03-10 15:58:28 +0200

    DailyChallenge still don't understand why not working

--- Week_15/Day_2/DailyChallenge/DailyChallangeServerAndClient/client/src/App.jsx
+++ Week_15/Day_2/DailyChallenge/DailyChallangeServerAndClient/client/src/App.jsx
@@ -1,40 +1,41 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
 
-class App extends React.Component{
-  constructor(){
+class App extends React.Component {
+  constructor() {
     super();
     this.state = {
       inputValue: '',
       serverResponse: ''
-    }
-
-    handleSubmit = async(e) => {
-      e.preventDefault();
-      try{
-        const response = await fetch('/api/world', {
-          method: 'POST',
-          headers:{
-          'Content-Type' : 'application/json'
-          },
-          body: JSON.stringify({value: this.state.inputValue })
-        });
-          const data = await response.text();
-          this.setState({ serverResponse: data })
-      }catch(error){
-        console.log('error' , error);
-      }
-    };
-    handleChange = (e) =>{
-      this.setState({inputValue: e.target.value});
     };
   }
-  render(){
-    return(
-      <>
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('/api/world', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ value: this.state.inputValue })
+      });
+      const data = await response.text();
+      this.setState({ serverResponse: data });
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  handleChange = (e) => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  render() {
+    return (
       <div>
         <h1>ReactApp</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input type='text' value={this.state.inputValue} onChange={this.handleChange} />
           <button type='submit'>Submit</button>
         </form>
@@ -42,9 +43,8 @@ class App extends React.Component{
           <p>Server response: {this.state.serverResponse}</p>
         )}
       </div>
-      </>
-    )
+    );
   }
 }
 
-export default App
+export default App;
