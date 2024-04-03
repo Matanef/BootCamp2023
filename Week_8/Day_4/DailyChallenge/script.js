12b9d8c4632e620e289aa92f25f00149bc8db55c
Matanef <efratimatan@gmail.com>
2024-04-04 00:00:51 +0300

    Week 8 Day 4 Daily Challenge done

---
+++ Week_8/Day_4/DailyChallenge/script.js
@@ -0,0 +1,42 @@
+function getCarHonda(carInventory) {
+  for (let car of carInventory) {
+    if (car.car_make === "Honda") {
+      return `This is a ${car.car_make} ${car.car_model} from ${car.car_year}.`;
+    }
+  }
+  return "No Honda car found in the inventory.";
+}
+const inventory = [
+  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+  { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+  {
+    id: 4,
+    car_make: "Land Rover",
+    car_model: "Defender Ice Edition",
+    car_year: 2010,
+  },
+  { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+];
+
+console.log(getCarHonda(inventory));
+
+function sortCarInventoryByYear(carInventory) {
+  carInventory.sort((car1, car2) => car1.car_year - car2.car_year);
+  return carInventory;
+}
+
+const inventory2 = [
+  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+  { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+  {
+    id: 4,
+    car_make: "Land Rover",
+    car_model: "Defender Ice Edition",
+    car_year: 2010,
+  },
+  { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+];
+
+console.log(sortCarInventoryByYear(inventory2));
