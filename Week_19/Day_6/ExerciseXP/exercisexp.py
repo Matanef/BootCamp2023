3d73514a22020f09b9d4391b9fae4a0974db5b4b
Matanef <efratimatan@gmail.com>
2024-04-06 03:08:28 +0300

    Exercise XP done

--- Week_19/Day_6/ExerciseXP/exercisexp.py
+++ Week_19/Day_6/ExerciseXP/exercisexp.py
@@ -47,3 +47,147 @@ for member, age in family.items():
 print("price", price)
 sum_price = sum(price)
 print(sum_price)
+
+# group = int(input("how many are you going to be? "))
+# for member in range(group):
+#     age = int(input("Please enter an age of a member of your group: "))
+#     print(age)
+#     if age in kid:
+#         price.append(0)
+#     elif age in young:
+#         price.append(10)
+#     else:
+#         price.append(15)
+
+# print(price)
+# sum_price = sum(price)
+# print(sum_price)
+
+# Exercise 3: Zara
+
+brand = {
+    "name": "Zara", 
+    "creation_date": 1975,
+    "creator_name": "Amancio Ortega Gaona", 
+    "type_of_clothes": ["men", "women", "children", "home"], 
+    "international_competitors": ["Gap", "H&M", "Benetton"],
+    "number_stores": 7000,
+    "major_color":
+        {
+        "France": "blue", 
+        "Spain": "red", 
+        "US": {
+            "pink",
+            "green"}
+           }
+}
+print(brand)
+
+brand["number_stores"]= 2
+print(brand["number_stores"])
+print(f"Zara's Customers are a mixture between {brand["type_of_clothes"][0]}, {brand["type_of_clothes"][1]} and {brand["type_of_clothes"][2]}")
+
+brand["country_creation"] = "Spain"
+# print(brand)
+
+for key, value in brand.items():
+    if key == "international_competitors":
+        brand["international_competitors"].append("Desigual")
+print(brand)
+print(brand["international_competitors"])
+
+del brand["creation_date"]
+print(brand)
+
+last_comp_index= int(len(brand["international_competitors"]))
+print(last_comp_index)
+last_competitor = brand["international_competitors"][last_comp_index-1]
+print(last_competitor)
+major_color_us = brand["major_color"]["US"]
+print(major_color_us)
+
+key_amount = []
+for key, value in brand.items():
+    key_amount.append(key)
+print(key_amount)
+print(len(key_amount))
+    # print(key)
+
+for key in brand["major_color"]:
+    key_amount.append(key)
+    print(key)
+print(f"Total amount of Keys including sub dicionaries: {len(key_amount)}")
+print(key_amount)
+
+more_on_zara = {
+    "creation_date": 1975, 
+    "number_stores": 10000
+}
+
+for key , value in more_on_zara.items():
+    brand[key] = value
+
+print(brand)
+print(brand["number_stores"])
+
+# i'm sorry i don't understand the question:
+# 14. Print the value of the key number_stores. What just happened ?
+
+# Exercise 4 : Disney characters
+users = ["Mickey","Minnie","Donald","Ariel","Pluto"]
+index = [0,1,2,3,4]
+
+
+disney_users_A= {}
+for key in users:
+    for value in index:
+        disney_users_A[key] = value
+        index.remove(value)
+        break
+    
+print(disney_users_A)
+
+users2 = ["Mickey","Minnie","Donald","Ariel","Pluto"]
+disney_users_A2= {}
+
+for index, user in enumerate(users2, start=0):
+    disney_users_A2[user] = index
+
+
+print(disney_users_A2)
+
+disney_users_B= {}
+for index, user in enumerate(users):
+    disney_users_B[index] = user
+
+print(disney_users_B)
+
+sortednames=sorted(disney_users_A2.keys())
+
+disney_users_C = {}
+for index, name in sorted(enumerate(sortednames)):
+    disney_users_C[index] = name
+
+print(sortednames)
+print(disney_users_C)
+
+disney_users_A3= {}
+
+for index, user in enumerate(users2, start=0):
+    if "i" in user:
+        disney_users_A3[user] = index
+
+
+print(disney_users_A3)
+
+disney_users_A4= {}
+
+for index, user in enumerate(users, start=0):
+    # print(user[0], user)
+    if user[0].lower() == "m" or user[0].lower() == "p":
+        # print(user[0], index)
+        disney_users_A4[user] = index
+
+
+print(disney_users_A4)
+# print(users2)
